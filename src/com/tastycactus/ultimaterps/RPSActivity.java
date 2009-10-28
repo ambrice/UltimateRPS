57445885d441d1d1ac6e031ba2e2808cff2b4bbd
Aaron Brice <aaron.brice@gmail.com>
2009-10-27 19:56:52 -0700

    Initial empty project

---
+++ src/com/tastycactus/ultimaterps/RPSActivity.java
@@ -0,0 +1,15 @@
+package com.tastycactus.ultimaterps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RPSActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
